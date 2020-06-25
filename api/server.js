2be8c4de39b2c43084cf83dd3fe8c426d54f4944
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-06-25 08:13:29 -0700

    added starter files

--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const authRoute = require("../routes/authRoute");
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRoute);
 
 module.exports = server;
